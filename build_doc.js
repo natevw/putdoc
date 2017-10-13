462a4c38e65524b3a6801dc5796ac95c573db60d
Nathan Vander Wilt <nate@calftrail.com>
2017-10-13 15:44:41 -0700

    refactor path handling in support of ignore files (#1)
    Specifically, this keeps the relative path of files separate from the ddoc_dir root path.

--- build_doc.js
+++ build_doc.js
@@ -10,43 +10,45 @@ module.exports = function (ddoc_dir, opts) {
   
   function objFromDir(dir, lvl) {
     var obj = {};
-    fs.readdirSync(dir).forEach(function (file) {
-        var path = p.join(dir,file),
-            type = fs.statSync(path);
+    fs.readdirSync(p.join(ddoc_dir,dir)).forEach(function (file) {
+        var rel_path = p.join(dir,file),
+            abs_path = p.join(ddoc_dir,rel_path),
+            type = fs.statSync(abs_path);
         if (file[0] === '.') return;
         else if (lvl === 0 && type.isDirectory() && file === '_attachments') {
          obj._attachments = {};
-         addAttsFromDir(obj._attachments, path, '');
+         addAttsFromDir(obj._attachments, rel_path, '');
         }
         else if (type.isDirectory() && opts.excludeDirs.indexOf(file) === -1) {
-          obj[file] = objFromDir(path, lvl+1);
+          obj[file] = objFromDir(rel_path, lvl+1);
         }
         else if (type.isFile()) {
           var ext = p.extname(file);
-          var data = fs.readFileSync(path, 'utf8');
+          var data = fs.readFileSync(abs_path, 'utf8');
           if (ext === '.json') {
             data = JSON.parse(data);
           }
           obj[p.basename(file,ext)] = data
         } else {
-            console.warn("Skipping ", path);
+            console.warn("Skipping ", rel_path);
         }
     });
     return obj;
   }
   function addAttsFromDir(atts, dir, pre) {
-    fs.readdirSync(dir).forEach(function (file) {
+    fs.readdirSync(p.join(ddoc_dir,dir)).forEach(function (file) {
       if (file[0] === '.') return;
       var key = p.join(pre,file),
-          path = p.join(dir,file),
-          type = fs.statSync(path);
-      if (type.isDirectory()) addAttsFromDir(atts, path, key);
+          rel_path = p.join(dir,file),
+          abs_path = p.join(ddoc_dir,rel_path),
+          type = fs.statSync(abs_path);
+      if (type.isDirectory()) addAttsFromDir(atts, rel_path, key);
       else atts[key] = {
         content_type: mime.lookup(file),
-        data: fs.readFileSync(path).toString('base64')
+        data: fs.readFileSync(abs_path).toString('base64')
       };
     });
   }
   
-  return objFromDir(ddoc_dir, 0);   
+  return objFromDir('', 0);
 }
