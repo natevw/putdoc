2a5c16e0f4a498fb12599c1cbe4a4f6b1d81b7b4
Nathan Vander Wilt <nate@calftrail.com>
2017-10-13 16:51:34 -0700

    get rid of excludeDirectories option, add ignore option with couchappignore loading by default, fixes #1

--- build_doc.js
+++ build_doc.js
@@ -5,8 +5,34 @@ var fs = require('fs'),
 
 module.exports = function (ddoc_dir, opts) {
   opts = extend({
-    excludeDirs: ['packages', 'node_modules'],
+    ignore: '.couchappignore'     // string: path to JSON ignores list, array: provide list directly, fn: used to test
   }, opts)
+  
+  if (typeof opts.ignore === 'string') try {
+    var ignore_file = p.join(ddoc_dir, opts.ignore);
+    opts.ignore = loadData(ignore_file, {json:true});
+  } catch (err) {
+    if (err.code === 'ENOENT') opts.ignore = [];
+    else throw err;
+  }
+  if (Array.isArray(opts.ignore)) {
+    var exclude_list = opts.ignore.map(function (s) { return new RegExp(s); });
+    opts.ignore = function (rel_path) {
+      return exclude_list.some(function (re) {
+        return re.test(rel_path);
+      });
+    }
+  }
+  
+  function loadData(abs_path, opts) {
+    opts = extend({json:false}, opts)
+    
+    var data = fs.readFileSync(abs_path, 'utf8');
+    if (opts.json) {
+      data = JSON.parse(data);
+    }
+    return data;
+  }
   
   function objFromDir(dir, lvl) {
     var obj = {};
@@ -14,21 +40,18 @@ module.exports = function (ddoc_dir, opts) {
         var rel_path = p.join(dir,file),
             abs_path = p.join(ddoc_dir,rel_path),
             type = fs.statSync(abs_path);
-        if (file[0] === '.') return;
+        if (file[0] === '.' || opts.ignore(rel_path)) return;
         else if (lvl === 0 && type.isDirectory() && file === '_attachments') {
          obj._attachments = {};
          addAttsFromDir(obj._attachments, rel_path, '');
         }
-        else if (type.isDirectory() && opts.excludeDirs.indexOf(file) === -1) {
+        else if (type.isDirectory()) {
           obj[file] = objFromDir(rel_path, lvl+1);
         }
         else if (type.isFile()) {
-          var ext = p.extname(file);
-          var data = fs.readFileSync(abs_path, 'utf8');
-          if (ext === '.json') {
-            data = JSON.parse(data);
-          }
-          obj[p.basename(file,ext)] = data
+          var ext = p.extname(file),
+              key = p.basename(file,ext);
+          obj[key] = loadData(abs_path, {json:(ext === '.json')})
         } else {
             console.warn("Skipping ", rel_path);
         }
@@ -37,12 +60,12 @@ module.exports = function (ddoc_dir, opts) {
   }
   function addAttsFromDir(atts, dir, pre) {
     fs.readdirSync(p.join(ddoc_dir,dir)).forEach(function (file) {
-      if (file[0] === '.') return;
       var key = p.join(pre,file),
           rel_path = p.join(dir,file),
           abs_path = p.join(ddoc_dir,rel_path),
           type = fs.statSync(abs_path);
-      if (type.isDirectory()) addAttsFromDir(atts, rel_path, key);
+      if (file[0] === '.' || opts.ignore(rel_path)) return;
+      else if (type.isDirectory()) addAttsFromDir(atts, rel_path, key);
       else atts[key] = {
         content_type: mime.lookup(file),
         data: fs.readFileSync(abs_path).toString('base64')
