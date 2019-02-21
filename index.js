ddd9669486aa3fcb7147e4b1598b7079fa8924ca
Nathan Vander Wilt <nate@calftrail.com>
2019-02-21 15:59:59 -0800

    support multiple document upload in CLI tool, towards #3

--- index.js
+++ index.js
@@ -11,16 +11,27 @@ if (process.argv.length < 4) {
 var doc_folder = process.argv[2],
     post_url = process.argv[3];
 
-var api = fermata.json(post_url),
-    doc = buildDoc(doc_folder),
-    doc_url = api([doc._id]);
-doc_url.get(function (e,oldDoc) {
-  if (e && e.status !== 404) console.error("Error checking current doc:", e, arguments);
-  else {
-    if (!e) doc._rev = oldDoc._rev;
-    doc_url.put(doc, function (e,d) {
-      if (e) console.error("Error uploading doc:", e);
-      else console.log(d);
-    });
+var docs = [];
+function extractDocs(doc) {
+  docs.push(doc);
+  if (doc._docs) {
+    doc._docs.forEach(extractDocs);
+    delete doc._docs;
   }
+}
+extractDocs(buildDoc(doc_folder));
+
+var api = fermata.json(post_url);
+docs.forEach(function (doc) {
+  var doc_url = api([doc._id]);
+  doc_url.get(function (e,oldDoc) {
+    if (e && e.status !== 404) console.error("Error checking current doc:", e, arguments);
+    else {
+      if (!e) doc._rev = oldDoc._rev;
+      doc_url.put(doc, function (e,d) {
+        if (e) console.error("Error uploading doc:", e);
+        else console.log(d);
+      });
+    }
+  });
 });
